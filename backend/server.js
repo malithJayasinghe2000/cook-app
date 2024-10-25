1b9355af18f61e1ad2e134da41abbf517458e228
Malith Jayasinghe <malith2jayasinghe@gmail.com>
2024-10-25 14:45:07 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL  ,
-    credentials: true, 
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
 }));
 app.use(express.json());
 app.use(cookieParser()); // Add this line
@@ -26,6 +26,14 @@ app.use('/api/auth', authRoutes);
 app.use('/api/meals', mealRoutes);
 app.use('/api/user', userRoutes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
